49fdfda6c9c9302daaedcdb704a728b1b6f92012
RussRussBus <russell@CARLOSs-MacBook-Pro.local>
2025-03-23 15:22:31 -0700

    creating random genome

---
+++ rand_genome.py
@@ -0,0 +1,20 @@
+import random
+import argparse
+
+parser = argparse.ArgumentParser(description='creates random genome')
+parser.add_argument('genome_size', type=int, help='size of genome')
+parser.add_argument('-s','--seed', type=int, help='set random seed')
+args = parser.parse_args()
+
+genome_size = args.genome_size
+
+random.seed(args.seed)
+
+def make_genome(size):
+    genome = []
+    for pos in range(size):
+        genome.append(random.choice('ACGT'))
+    genome = ''.join(genome)
+    return genome
+
+print(make_genome(genome_size))
